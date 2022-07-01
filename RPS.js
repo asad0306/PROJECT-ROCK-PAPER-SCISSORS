13c410b7b89c385fec393d1d6bcfce22167918e5
asad0306 <qaisansari46@gmail.com>
2022-07-01 04:38:49 +0100

    Computerplay section commit

---
+++ RPS.js
@@ -0,0 +1,13 @@
+const computerPlay = "Rock";
+const computerPlay1 = "Paper";
+const computerPlay2 = "Scissors";
+
+function computerPla () {
+    if (computerPlayS) {
+        console.log("Rock");
+    } else if (computerPlay1) {
+        console.log("Paper");
+    } else {
+        console.log("Scissors");
+    }
+}
